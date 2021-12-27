11e5838be41dcf0a4bf13178b2c17ee5ffba355d
wmboy <95390981+wmboy@users.noreply.github.com>
2021-12-27 19:35:06 +0800

    Delete js directory

--- js/prbug.js
+++
@@ -1,6 +0,0 @@
-var ody=document.getElementById('ydmc');
-if (yaudio.paused) {
-ody.className = 'mdi mdi-music-note-off-outline mdi-24px';
-} else {
-ody.className = 'mdi mdi-music-note-outline mdi-24px';
-}
